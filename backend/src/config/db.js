a78eee242a92aa124f6093fcaea27e5057400020
LaibaFirdouse <laibafirshaikh@gmail.com>
2026-03-12 13:18:13 +0530

    fix db connection for render

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,25 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
+// pool.connect()
+//     .then(() => console.log("PostgreSQL connected"))
+//     .catch(err => console.error("DB connection failed", err));
+
+// module.exports = pool;
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.connect()
