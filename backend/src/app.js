0527f0da8cfb9763a829263058a6fb4d6ed2898d
LaibaFirdouse <laibafirshaikh@gmail.com>
2026-03-12 13:59:38 +0530

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,21 @@ const serviceRoutes = require("./routes/service.routes");
 
 const app = express();
 
+// app.use(
+//     cors({
+//         origin: "http://localhost:5173",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         credentials: true
+//     })
+// );
+
 app.use(
     cors({
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:5173",
+            "https://flowserve-six.vercel.app"
+        ]
     })
 );
 
@@ -17,3 +27,6 @@ app.use(express.json());
 app.use("/api/services", serviceRoutes);
 
 module.exports = app;
+
+const cors = require("cors");
+
