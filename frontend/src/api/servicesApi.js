075814a164470bb4c4bb33ecd9f0aea01230bc7b
LaibaFirdouse <laibafirshaikh@gmail.com>
2026-03-12 13:49:58 +0530

    update api url

--- frontend/src/api/servicesApi.js
+++ frontend/src/api/servicesApi.js
@@ -1,3 +1,22 @@
+// export async function fetchServices({ page = 1, limit = 10, search = "" }) {
+//   const params = new URLSearchParams({
+//     page,
+//     limit,
+//     search
+//   });
+
+//   const response = await fetch(
+//     `http://localhost:5000/api/services?${params.toString()}`
+//   );
+
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch services");
+//   }
+
+//   return response.json();
+// }
+const API = import.meta.env.VITE_API_URL;
+
 export async function fetchServices({ page = 1, limit = 10, search = "" }) {
   const params = new URLSearchParams({
     page,
@@ -5,9 +24,7 @@ export async function fetchServices({ page = 1, limit = 10, search = "" }) {
     search
   });
 
-  const response = await fetch(
-    `http://localhost:5000/api/services?${params.toString()}`
-  );
+  const response = await fetch(`${API}/services?${params}`);
 
   if (!response.ok) {
     throw new Error("Failed to fetch services");
